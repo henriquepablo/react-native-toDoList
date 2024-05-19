6537f958aa7aad39b80112ba08fdc472245df164
henriquepablo <pablo269704@gmail.com>
2024-05-19 18:29:32 -0300

    feat: add textInput and button for add news tasks int the modal

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Image } from "react-native";
 
-import { ViewModal, ContainerModal } from "../styles/ModalStyle";
+import { ViewModal, ContainerModal, ViewTextInput, Input, ViewTouchableModal } from "../styles/ModalStyle";
 import { ViewTouchable, ViewModalBtn } from "../styles/TaksStyle";
 
 import MyContext from "../Context";
@@ -13,6 +13,18 @@ export default MyModal = (props) => {
     return (
         <ContainerModal>
             <ViewModal background={isDark}>
+
+                <ViewTextInput >
+                    
+                    <Input colorLetter={isDark} backgroundInput={isDark}/>
+
+                    <ViewTouchableModal >
+                        <ViewModalBtn background={isDark}>
+                            <Image source={isDark == true ? require('../img/add.png') : require('../img/addBlack.png')} />
+                        </ViewModalBtn >
+                    </ViewTouchableModal >
+                    
+                </ViewTextInput>
 
                 <ViewTouchable onPress={props.close}>
                     <ViewModalBtn background={isDark}>
--- src/styles/ModalStyle.js
+++ src/styles/ModalStyle.js
@@ -9,3 +9,18 @@ export const ViewModal = styled.View`
     height: 333px;
     border-radius: 30px 30px 0px 0px;   
 `
+export const ViewTextInput = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+`
+export const Input = styled.TextInput`
+    background-color: ${(props) => props.backgroundInput ? '#3F414D' : '#fff'};
+    width: 232px;
+    margin: 33px 14px;
+    border-radius: 10px;
+    color: ${(props) => props.colorLetter ? '#fff' : '#000'};
+    font-size: 17px;
+    padding: 10px;
+`
+export const ViewTouchableModal = styled.TouchableOpacity``;
