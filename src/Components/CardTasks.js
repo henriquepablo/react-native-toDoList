35dad46023d80dc1f37d15e8d81f285c86516350
henriquepablo <pablo269704@gmail.com>
2024-05-25 09:52:34 -0300

    feat: adding method delete task

--- src/Components/CardTasks.js
+++ src/Components/CardTasks.js
@@ -1,6 +1,6 @@
 import { Image, Switch, TouchableOpacity, View } from "react-native";
 import { ViewCard, TextTasks } from "../styles/CardStyle";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import MyContext from "../Context";
 import DropShadow from "react-native-drop-shadow";
 import { styleCard } from "../styles/CardStyle";
@@ -9,9 +9,31 @@ import { styleCard } from "../styles/CardStyle";
 export default Card = (props) => {
 
     const [isEnabled, setIsEnabled] = useState(false);
+    const [idTask, setIdTask] = useState();
 
     const {isDark} = useContext(MyContext);
     
+    useEffect(() => {
+        deleteTask();
+    },[]);
+
+    const deleteTask = () => {
+        setIdTask(props.data.id);
+        fetch(`http://10.0.2.2:8080/tasks/${idTask}`, {
+            method: 'DELETE',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json" 
+            },
+            body: JSON.stringify({})
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+        })
+        .catch(err => console.log(err));
+    }
+
     return (
         <DropShadow style={styleCard}>
             <ViewCard backgroundCard={isDark}> 
@@ -25,7 +47,7 @@ export default Card = (props) => {
                     <Image source={require('../img/edit.png')} />
                 </TouchableOpacity>
                 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={deleteTask}>
                     <Image source={require('../img/trash.png')} />
                 </TouchableOpacity>
                 
